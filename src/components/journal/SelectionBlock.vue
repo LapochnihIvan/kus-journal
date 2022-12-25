00b4ed46ed2a4c034c3c4f701730e175f4483ff3
Pashs-ba <pashs.ba@gmail.com>
2022-12-25 15:43:20 +0300

    remove plan

--- src/components/journal/SelectionBlock.vue
+++ src/components/journal/SelectionBlock.vue
@@ -1,14 +1,11 @@
 <script setup>
-defineProps(['modelValue'])
-defineEmits(['update:modelValue'])
 
 </script>
 
 <template>
     <div class="row">
         <div class="col-2">
-            <select class="form-select " aria-label="Default select example" :value="modelValue"
-                    @input="$emit('update:modelValue', $event.target.value)">
+            <select class="form-select " disabled>
                 <option selected disabled value="-1">Предмет</option>
                 <option value="1">Тест</option>
             </select>
@@ -16,11 +13,6 @@ defineEmits(['update:modelValue'])
         <div class="col-1">
             <select class="form-select " aria-label="Default select example" disabled>
                 <option selected>Класс</option>
-            </select>
-        </div>
-        <div class="col-2">
-            <select class="form-select " aria-label="Default select example" disabled>
-                <option selected>Учебный план</option>
             </select>
         </div>
     </div>
