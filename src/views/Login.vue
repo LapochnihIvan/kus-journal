04c6388952a8a26336c9380c5b37fed0781fc3f7
Pashs-ba <pashs.ba@gmail.com>
2022-12-26 09:29:47 +0300

    add authorizing system

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,31 @@
 <script setup>
 import "bootstrap-icons/font/bootstrap-icons.css";
 import router from "@/router";
+import {ref} from "vue";
+import axios from "axios";
+
+
+const username = ref("");
+const password = ref("");
+
+function login() {
+  console.log(username.value)
+  axios.post("http://194.87.206.68/login",
+      {
+        "login": username.value,
+        "password": password.value
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      }
+  ).then((response) => {
+    if (response.status === 200) {
+      router.push("/journal")
+    }
+  })
+}
 </script>
 
 
@@ -9,22 +34,21 @@ import router from "@/router";
     <div class="col-4 card ">
       <div class="card-body">
 
-          <div class="form-group my-3">
-            <label for="username" class="form-label">Имя пользователя</label>
-            <input type="text" name="username" id="username" class="form-control">
-          </div>
-          <div class="form-group">
-            <label for="username" class="form-label">Пароль</label>
+        <div class="form-group my-3">
+          <label for="username" class="form-label">Имя пользователя</label>
+          <input type="text" name="username" id="username" class="form-control" v-model="username">
+        </div>
+        <div class="form-group">
+          <label for="username" class="form-label">Пароль</label>
 
-            <div class="input-group my-3" id="show_hide_password">
-              <input type="password" name="password" id="pass" class="form-control"
-                     style="margin-top: 0px">
-              <button class="btn btn-secondary" type="button" onclick="">
-                <i id="icon" class="bi bi-eye-fill"></i>
-              </button>
-            </div>
+          <div class="input-group my-3" id="show_hide_password">
+            <input type="password" name="password" id="pass" class="form-control" v-model="password">
+            <button class="btn btn-secondary" type="button" onclick="">
+              <i id="icon" class="bi bi-eye-fill"></i>
+            </button>
           </div>
-          <button class="btn btn-primary my-3" v-on:click="router.push('/journal')">Зайти</button>
+        </div>
+        <button class="btn btn-primary my-3" @click="login">Зайти</button>
       </div>
     </div>
   </div>
