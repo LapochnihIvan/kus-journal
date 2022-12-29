2a3d1a1051e75b54751769d5aee41e08dc81bf21
Pashs-ba <pashs.ba@gmail.com>
2022-12-29 17:53:17 +0300

    check active login

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import {login_required} from "@/utils/authorization";
 import {home_routes} from "@/router/home";
+import {login_before} from "@/utils/authorization";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +15,7 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
+      beforeEnter: login_before,
       component: ()=>import("@/views/system_functions/Login.vue")
     },
     {
--- src/utils/authorization.js
+++ src/utils/authorization.js
@@ -8,3 +8,14 @@ export const login_required = () =>{
         return {name: "login"};
     }
 }
+
+export const login_before = (to, from) =>{
+    try {
+        let user_data = JSON.parse(localStorage.getItem("user"));
+        if (user_data) {
+            return from;
+        }
+    }catch (e){
+        return {name: "login"};
+    }
+}
