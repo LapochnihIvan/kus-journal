2f0845ca922598c2562eaf58c3f1a175596b2db7
Pashs-ba <pashs.ba@gmail.com>
2022-12-29 17:46:44 +0300

    move images inside src

--- src/views/system_functions/HomeFiller.vue
+++ src/views/system_functions/HomeFiller.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="d-flex justify-content-center align-items-center flex-column" style="height: 100%">
 
-   <img alt="Тут должен быть крутой логотип, но его почему то нет...." src="public/Magic_cat.svg" height="200" width="200">
+   <img alt="Тут должен быть крутой логотип, но его почему то нет...." src="../../static/Magic_cat.svg" height="200" width="200">
     <h3 class="mt-5">Что бы начать работать выберите любой элемент слева :)</h3>
   </div>
 </template>
