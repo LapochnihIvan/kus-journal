6d8040c1863f7f5f1019a88650f2303bd57765d6
Pashs-ba <pashs.ba@gmail.com>
2023-01-02 19:20:14 +0300

    make SelectionBlock

--- src/components/journal/SelectionBlock.vue
+++ src/components/journal/SelectionBlock.vue
@@ -1,19 +1,55 @@
 <script setup>
+import {computed, ref, watch} from "vue";
 
+const props = defineProps(["journals"])
+const emit = defineEmits(["setJournal"])
+
+const subjects = computed(() => {
+  let subjects = []
+  for (let journal of props.journals) {
+    if (!subjects.includes(journal.subject)) {
+      subjects.push(journal.subject)
+    }
+  }
+  return subjects
+})
+const selected_subject = ref('')
+
+const grades = computed(() => {
+  let grades = []
+  for (let journal of props.journals) {
+    if (!grades.includes(journal.group_id) && journal.subject === selected_subject.value) {
+      grades.push(journal.group_id)
+    }
+  }
+  return grades
+})
+const selected_grade = ref('')
+
+watch(selected_grade, (new_grade) => {
+  const selected = props.journals.filter((journal) => {
+    return journal.group_id === selected_grade.value && journal.subject === selected_subject.value
+  })[0]
+
+  if (selected) {
+    emit("setJournal", selected);
+  }
+})
 </script>
 
 <template>
-    <div class="row">
-        <div class="col-2">
-            <select class="form-select " disabled>
-                <option selected disabled value="-1">Предмет</option>
-                <option value="1">Тест</option>
-            </select>
-        </div>
-        <div class="col-2">
-            <select class="form-select " aria-label="Default select example" disabled>
-                <option selected>Класс</option>
-            </select>
-        </div>
+  <div class="row my-3">
+    <div class="col-2">
+      <select class="form-select " v-model="selected_subject">
+        <option v-for="subject in subjects" :value="subject">{{ subject }}</option>
+      </select>
     </div>
+    <div class="col-2">
+      <select class="form-select " aria-label="Default select example"
+              :disabled="selected_subject===''"
+              v-model="selected_grade">
+        <option v-for="grade in grades" :value="grade">{{ grade }}</option>
+      </select>
+    </div>
+  </div>
 </template>
--- src/views/journal/JournalView.vue
+++ src/views/journal/JournalView.vue
@@ -1,10 +1,27 @@
 <script setup>
+import axios from "axios";
+import {ref} from "vue";
+import {URL} from "@/utils/config";
+import SelectionBlock from "@/components/journal/SelectionBlock.vue";
 
+const journals = ref([])
+const selected = ref();
+let user = JSON.parse(localStorage.getItem("user"))
+
+axios.get(URL+"/journal/"+user.id, {
+  headers: {
+    token: user.token
+  }
+}).then((response)=>{
+  journals.value = response.data["journals"]
+})
 </script>
 
 
 <template>
-Journal Template
+
+  <SelectionBlock :journals="journals" @set-journal="(journal)=>{selected = journal}"/>
+  {{ selected }}
 </template>
 
 <style scoped>
