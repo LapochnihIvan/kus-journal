eef88c75cc06ee1220a031f5fb3cb07676e94cbb
Pashs-ba <pashs.ba@gmail.com>
2023-01-03 17:52:10 +0300

    make form

--- src/components/journal/LessonModal.vue
+++ src/components/journal/LessonModal.vue
@@ -5,7 +5,8 @@ const props = defineProps(["lesson"])
 
 
 <template>
-  <div class="modal fade" id="lesson" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="lesson" aria-hidden="true">
+  <div class="modal fade" id="lesson" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+       aria-labelledby="lesson" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
@@ -13,11 +14,23 @@ const props = defineProps(["lesson"])
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          {{lesson}}
+          <div class="mb-3">
+            <label class="form-label">Дата</label>
+            <p>{{lesson.date}}</p>
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Тип контроля занятия</label>
+            <input type="text" class="form-control" :value="lesson.control">
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Домашняя работа</label>
+            <textarea type="text" class="form-control">{{ lesson.homework }}</textarea>
+          </div>
         </div>
+
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Understood</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+          <button type="button" class="btn btn-primary">Сохранить</button>
         </div>
       </div>
     </div>
