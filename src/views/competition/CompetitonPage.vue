66e2499fe5c5615fdfa5228a9858235a44392c74
Pashs-ba <pashs.ba@gmail.com>
2023-04-02 14:36:28 +0300

    fix

--- src/views/competition/CompetitonPage.vue
+++ src/views/competition/CompetitonPage.vue
@@ -6,9 +6,9 @@ import moment from "moment";
 
 const competitions = ref()
 
-axios.get(URL+"/get/if/user_competition[competition_id[id;name;start_time]]/user_id="+JSON.parse(localStorage.getItem("user")).id).then((response)=>{
+axios.get(URL + "/get/if/user_competition[competition_id[id;name;start_time]]/user_id=" + JSON.parse(localStorage.getItem("user")).id).then((response) => {
   competitions.value = response.data;
-  competitions.value.user_competitions.forEach((el)=>{
+  competitions.value.user_competitions.forEach((el) => {
     let true_date = new Date()
     let date = el.competition.start_time.split(' ')[0]
     true_date.setFullYear(date.split('-')[0], date.split('-')[1], date.split('-')[2])
@@ -18,27 +18,41 @@ axios.get(URL+"/get/if/user_competition[competition_id[id;name;start_time]]/user
     true_date.setSeconds(time.split(':')[2])
     el.competition.start_time = true_date
   })
+  if (competitions.value.user_competitions.lenght === undefined) {
+    const d = new Date("2023-05-02T06:00:00.632Z")
+    competitions.value.user_competitions.push({
+      "competition": {
+        "id": 1,
+        "name": "Пробный тур",
+        "start_time": d
+      }
+    })
+  }
 })
 
-const now_time = computed(()=>{
+
+const now_time = computed(() => {
   return new Date()
 })
-const compare_time = (time)=>{
+const compare_time = (time) => {
   const c = new Date()
-  return c.getHours()>=time.getHours() && c.getDate()>=time.getDate()
+  return c.getHours() >= time.getHours() && c.getDate() >= time.getDate()
 }
 </script>
 
 <template>
-<div class=" d-flex justify-content-center align-items-center" style="height: 100%">
-  <div class="card m-2" v-for="competition in competitions.user_competitions" v-if="competitions">
-    <div class="card-body m-5 text-center">
-      <h5 class="card-title">{{ competition.competition.name }}</h5>
-      <router-link class="btn btn-outline-primary" :to="{name: 'task_list', params:{'c_id': competition.competition.id}}" v-if="compare_time(competition.competition.start_time)">Участвовать</router-link>
-      <a class="btn btn-outline-secondary" v-else>Соревнование закрыто</a>
+  <div class=" d-flex justify-content-center align-items-center" style="height: 100%">
+    <div class="card m-2" v-for="competition in competitions.user_competitions" v-if="competitions">
+      <div class="card-body m-5 text-center">
+        <h5 class="card-title">{{ competition.competition.name }}</h5>
+        <router-link class="btn btn-outline-primary"
+                     :to="{name: 'task_list', params:{'c_id': competition.competition.id}}"
+                     v-if="compare_time(competition.competition.start_time)">Участвовать
+        </router-link>
+        <a class="btn btn-outline-secondary" v-else>Соревнование закрыто</a>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 
