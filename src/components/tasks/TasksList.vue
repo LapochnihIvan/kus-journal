5969c9426c97ca74178663b6c780c8f8a2defaf5
Pashs-ba <pashs.ba@gmail.com>
2023-04-18 11:18:38 +0300

    фтщерук another fixes

--- src/components/tasks/TasksList.vue
+++ src/components/tasks/TasksList.vue
@@ -7,13 +7,13 @@ const store = useStore();
 
 <template>
   <ul class="list-group">
-<!--    <li class="list-group-item border-0 py-1" v-for="task in store.state.tasks.competition_list">-->
-<!--      <router-link style="text-decoration: none"-->
-<!--                   class="text-dark"-->
-<!--                   :to="{'name': 'problem_page', params:{'id': task.id}}">-->
-<!--        {{ task.name }}-->
-<!--      </router-link>-->
-<!--    </li>-->
+    <li class="list-group-item border-0 py-1" v-for="task in store.state.tasks.problems_list">
+      <router-link style="text-decoration: none"
+                   class="text-dark"
+                   :to="{'name': 'problem_page', params:{'id': task.id}}">
+        {{ task.name }}
+      </router-link>
+    </li>
     <li class="list-group-item border-0 mb-1"  v-for="task in store.state.tasks.questions_list" :class="task.sent?'bg-secondary bg-opacity-25 rounded-2':null">
       <router-link style="text-decoration: none"
                    class="text-dark"
